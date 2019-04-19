96afed3dda47565cef3585c6d370dcd4f029aa1e
Piotr Satała <piotr.satala@wp.pl>
2019-04-19 02:38:05 +0200

    Finish working on collisions

--- PROItheGame/PROItheGame/BasicFunctions.cpp
+++ PROItheGame/PROItheGame/BasicFunctions.cpp
@@ -41,7 +41,13 @@ void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, c
 	const double g = 0.001;
 
 	Player myPlayer((SCREEN_WIDTH - PLAYER_WIDTH) / 2, (SCREEN_HEIGHT - PLAYER_HEIGHT) / 2, PLAYER_HEIGHT, PLAYER_WIDTH, x, y, g);
-	RectangularObstacle firstObstacle((SCREEN_WIDTH - 200) / 2, (SCREEN_HEIGHT - 50) * 3 / 4, 50, 200);
+	RectangularObstacle firstObstacle((SCREEN_WIDTH - 400) / 2, (SCREEN_HEIGHT - 50) * 5 / 8, 50, 400);
+	RectangularObstacle secondObstacle(440, 100, 300, 30);
+	RectangularObstacle thirdObstacle(0, SCREEN_HEIGHT - 30, 30, SCREEN_WIDTH);
+	RectangularObstacle fourthObstacle(0, 0, SCREEN_HEIGHT, 15);
+	RectangularObstacle fifthObstacle(SCREEN_WIDTH - 15, 0, SCREEN_HEIGHT, 15);
+	RectangularObstacle sixthObstacle(0, 0, 15, SCREEN_WIDTH);
+
 	
 	
 	Uint32 startTime;
@@ -73,12 +79,22 @@ void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, c
 			myPlayer.moveRight();
 
 
-		myPlayer.checkCollision(&firstObstacle);
 		myPlayer.calculateNextPosition(TIME_BETWEEN_FRAMES);
-		
-		
+		myPlayer.checkCollision(&firstObstacle);
+		myPlayer.checkCollision(&secondObstacle);
+		myPlayer.checkCollision(&thirdObstacle);
+		myPlayer.checkCollision(&fourthObstacle);
+		myPlayer.checkCollision(&fifthObstacle);
+		myPlayer.checkCollision(&sixthObstacle);
+
+
 		clear(renderer);
 		firstObstacle.print(renderer);
+		secondObstacle.print(renderer);
+		thirdObstacle.print(renderer);
+		fourthObstacle.print(renderer);
+		fifthObstacle.print(renderer);
+		sixthObstacle.print(renderer);
 		myPlayer.print(renderer);
 
 		SDL_RenderPresent(renderer);
--- PROItheGame/PROItheGame/Player.cpp
+++ PROItheGame/PROItheGame/Player.cpp
@@ -18,7 +18,8 @@ Player::~Player()
 
 void Player::jump()
 {
-	playerMomentum.setYVelocity(yConstant);
+	if(contact[DOWN])
+		playerMomentum.setYVelocity(yConstant);
 }
 
 void Player::moveRight()
@@ -42,6 +43,10 @@ void Player::calculateNextPosition(const double timeDifference)
 
 	//updating horizontal position
 	xCoordinate += playerMomentum.getXVelocity() * timeDifference;
+
+	//reseting contact
+	for (int i = 0; i < 4; i++)
+		contact[i] = false;
 }
 
 void Player::print(SDL_Renderer* rendererToPrintOn)
@@ -54,17 +59,29 @@ void Player::print(SDL_Renderer* rendererToPrintOn)
 
 void Player::checkCollision(RectangularObstacle* obstacle)
 {
-	if (contact[UP] = checkCollisionSide(obstacle, UP))
+	bool contactWithThisObject[4] = { 0 };
+	if (contactWithThisObject[UP] = checkCollisionSide(obstacle, UP))
+	{
 		getPlayerMomentum()->setYVelocity(0);
-	if (contact[DOWN] = checkCollisionSide(obstacle, DOWN))
+		yCoordinate = obstacle->getYCoordinate() + obstacle->getObjectHeight();
+	}
+	if (contactWithThisObject[DOWN] = checkCollisionSide(obstacle, DOWN))
 	{
 		getPlayerMomentum()->setYVelocity(0);
 		yCoordinate = obstacle->getYCoordinate() - objectHeight;
 	}
-	if (contact[LEFT] = checkCollisionSide(obstacle, LEFT))
+	if (contactWithThisObject[LEFT] = checkCollisionSide(obstacle, LEFT))
+	{
 		getPlayerMomentum()->setXVelocity(0);
-	if (contact[RIGHT] = checkCollisionSide(obstacle, RIGHT))
+		xCoordinate = obstacle->getXCoordinate() + obstacle->getObjectWidth();
+	}
+	if (contactWithThisObject[RIGHT] = checkCollisionSide(obstacle, RIGHT))
+	{
 		getPlayerMomentum()->setXVelocity(0);
+		xCoordinate = obstacle->getXCoordinate() - objectWidth;
+	}
+	for (int i = 0; i < 4; i++)
+		contact[i] |= contactWithThisObject[i];
 
 }
 
@@ -73,34 +90,31 @@ bool Player::checkCollisionSide(RectangularObstacle* obstacle, Direction dir)
 	switch (dir)
 	{
 	case UP:
+		if (xCoordinate + objectWidth > obstacle->getXCoordinate() && xCoordinate < obstacle->getXCoordinate() + obstacle->getObjectWidth())
+			if (yCoordinate <= obstacle->getYCoordinate() + obstacle->getObjectHeight() && yCoordinate + objectHeight >= obstacle->getYCoordinate() + obstacle->getObjectHeight())
+				return true;
 		return false;
 		break;
 	
 	case DOWN:
 		if (xCoordinate + objectWidth > obstacle->getXCoordinate() && xCoordinate < obstacle->getXCoordinate() + obstacle->getObjectWidth())
-			if (yCoordinate + objectHeight > obstacle->getYCoordinate() && yCoordinate < obstacle->getYCoordinate())
+			if (yCoordinate + objectHeight >= obstacle->getYCoordinate() && yCoordinate <= obstacle->getYCoordinate())
 				return true;
 		return false;
 		break;
 	
 	case LEFT:
+		if (yCoordinate + objectHeight > obstacle->getYCoordinate() && yCoordinate < obstacle->getYCoordinate() + obstacle->getObjectHeight())
+			if (xCoordinate <= obstacle->getXCoordinate() + obstacle->getObjectWidth() && xCoordinate + objectWidth >= obstacle->getXCoordinate() + obstacle->getObjectWidth())
+				return true;
 		return false;
 		break;
 	
 	case RIGHT:
+		if (yCoordinate + objectHeight > obstacle->getYCoordinate() && yCoordinate < obstacle->getYCoordinate() + obstacle->getObjectHeight())
+			if (xCoordinate + objectWidth >= obstacle->getXCoordinate() && xCoordinate <= obstacle->getXCoordinate())
+				return true;
 		return false;
 		break;
 	}
-	/*if (UP == dir || DOWN == dir)
-	{
-		if (xCoordinate + objectWidth > obstacle->getXCoordinate() && xCoordinate < obstacle->getXCoordinate() + obstacle->getObjectWidth())
-			return true;
-		return false;
-	}
-	else
-	{
-		if (yCoordinate + objectHeight > obstacle->getYCoordinate() && yCoordinate < obstacle->getYCoordinate() + obstacle->getObjectHeight())
-			return true;
-		return false;
-	}*/
 }
