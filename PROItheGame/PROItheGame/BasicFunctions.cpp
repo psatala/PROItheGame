48be3454057ba75407cc953e20ae0d6d951e7fda
Piotr Satała <piotr.satala@wp.pl>
2019-05-05 19:30:13 +0200

    Implement reading level from file (insecure)

--- PROItheGame/PROItheGame/BasicFunctions.cpp
+++ PROItheGame/PROItheGame/BasicFunctions.cpp
@@ -31,6 +31,58 @@ bool init(SDL_Window** window, SDL_Renderer** renderer, const int SCREEN_HEIGHT,
 	return success;
 }
 
+void load(string pathToFile, HumanPlayer **myPlayer, vector<Obstacle*>*myObstacles, vector<Enemy*>*myEnemies)
+{
+	//*myPlayer = &HumanPlayer(0, 0, 0, 0, 0, 0, 0, 0, 0);
+	//myObstacles->push_back(&Obstacle());
+	fstream myFile;
+	myFile.open(pathToFile);
+
+	if (!myFile.is_open())
+		throw "Unable to open file!";
+	else
+	{
+		
+		char c;
+		int xPos;
+		int yPos;
+		unsigned int width;
+		unsigned int height;
+		bool canKill;
+		double xSpeed;
+		double ySpeed;
+		double gForce;
+		int tDist;
+		unsigned int tTime;
+		string behaviourType;
+
+		while (!myFile.eof())
+		{
+			
+			myFile >> c >> xPos >> yPos >> width >> height;
+			switch (c)
+			{
+			case 'P': //player
+				myFile >> xSpeed >> ySpeed >> gForce >> tDist >> tTime;
+				*myPlayer = new HumanPlayer(xPos, yPos, width, height, xSpeed, ySpeed, gForce, tDist, tTime);				
+				break;
+			case 'O': //obstacle
+				myFile >> canKill;
+				myObstacles->push_back(new Obstacle(xPos, yPos, width, height, canKill));
+				break;
+			case 'E': //enemy
+				myFile >> xSpeed >> ySpeed >> gForce >> tDist >> tTime >> behaviourType;
+				myEnemies->push_back(new Enemy(xPos, yPos, width, height, xSpeed, ySpeed, gForce, tDist, tTime, behaviourType));
+				break;
+			default:
+				cout << "Wrong character\n";
+				break;
+			}
+		}
+		myFile.close();
+	}
+}
+
 void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, const int SCREEN_WIDTH, const int PLAYER_HEIGHT, const int PLAYER_WIDTH, const double TIME_BETWEEN_FRAMES)
 {
 	bool quit = false;
@@ -42,9 +94,22 @@ void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, c
 	const int tDist = 200;
 	const unsigned int tTime = 1000;
 
-	HumanPlayer myPlayer((SCREEN_WIDTH - PLAYER_WIDTH) / 2, (SCREEN_HEIGHT - PLAYER_HEIGHT) / 2, PLAYER_WIDTH, PLAYER_HEIGHT, x, y, g, tDist, tTime);
-	
+	HumanPlayer* myPlayer;
+	vector <Obstacle*> myObstacles;
+	vector <Enemy*> myEnemies;
 
+	
+	try
+	{
+		load("Levels/Level01.txt", &myPlayer, &myObstacles, &myEnemies);
+	}
+	catch (string message)
+	{
+		cerr << message << endl;
+	}
+
+
+	/*HumanPlayer myPlayer((SCREEN_WIDTH - PLAYER_WIDTH) / 2, (SCREEN_HEIGHT - PLAYER_HEIGHT) / 2, PLAYER_WIDTH, PLAYER_HEIGHT, x, y, g, tDist, tTime);
 
 	vector <Obstacle*> myObstacles;
 	myObstacles.push_back(&Obstacle((SCREEN_WIDTH - 400) / 2, (SCREEN_HEIGHT - 50) * 5 / 8, 400, 50));
@@ -54,16 +119,17 @@ void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, c
 	myObstacles.push_back(&Obstacle(SCREEN_WIDTH - 15, 0, 15, SCREEN_HEIGHT));
 	myObstacles.push_back(&Obstacle(0, 0, SCREEN_WIDTH, 15));
 	myObstacles.push_back(&Obstacle(0, SCREEN_HEIGHT + 300, SCREEN_WIDTH, 400, true));
-	
 
 	vector <Enemy*> myEnemies;
 	myEnemies.push_back(&Enemy(100, 50, 25, 25, 0.5 * x, 0.5 * y, g, tDist, tTime, "JUMP"));
 	myEnemies.push_back(&Enemy(50, 420, 30, 30, 0.5 * x, 0.5 * y, g, tDist, tTime, "BOUNCE"));
-	myEnemies.push_back(&Enemy(0, -200, 150, 150, 0.5 * x, 0.5 * y, g, tDist, tTime));
+	myEnemies.push_back(&Enemy(0, -200, 150, 150, 0.5 * x, 0.5 * y, g, tDist, tTime));*/
 
 	vector<GameObject*> allObjects(myObstacles.begin(), myObstacles.end());
 	vector<GameObject*> auxiliaryVector(myEnemies.begin(), myEnemies.end());
 	allObjects.insert(allObjects.end(), auxiliaryVector.begin(), auxiliaryVector.end());
+
+
 
 	Uint32 startTime;
 	Uint32 endTime;
@@ -86,14 +152,14 @@ void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, c
 
 
 		//applying input for the player
-		myPlayer.applyInput(allObjects);
+		myPlayer->applyInput(allObjects);
 		
 		//applying behaviour for the enemies
 		for(vector <Enemy*>::iterator jt = myEnemies.begin(); jt != myEnemies.end(); ++jt)
 			(**jt).applyBehaviour();
 
 		//updating objects positions
-		myPlayer.calculateNextPosition(TIME_BETWEEN_FRAMES);
+		myPlayer->calculateNextPosition(TIME_BETWEEN_FRAMES);
 		for (vector <Enemy*>::iterator jt = myEnemies.begin(); jt != myEnemies.end(); ++jt)
 			(**jt).calculateNextPosition(TIME_BETWEEN_FRAMES);
 		
@@ -104,7 +170,7 @@ void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, c
 		
 		//player
 		for (vector<Obstacle*>::iterator it = myObstacles.begin(); it != myObstacles.end(); ++it)
-			myPlayer.checkCollision(*it);
+			myPlayer->checkCollision(*it);
 		
 		
 		//enemies
@@ -112,7 +178,7 @@ void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, c
 		{
 			for (vector<Obstacle*>::iterator it = myObstacles.begin(); it != myObstacles.end(); ++it)
 				(**jt).checkCollision(*it);
-			(**jt).checkCollision(&myPlayer);
+			(**jt).checkCollision(myPlayer);
 		}
 		
 
@@ -122,10 +188,10 @@ void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, c
 		
 		//printing
 		for (vector<Obstacle*>::iterator it = myObstacles.begin(); it != myObstacles.end(); ++it)
-			(**it).print(renderer, &myPlayer);
+			(**it).print(renderer, myPlayer);
 		for (vector <Enemy*>::iterator jt = myEnemies.begin(); jt != myEnemies.end(); ++jt)
-			(**jt).print(renderer, &myPlayer);
-		myPlayer.print(renderer);
+			(**jt).print(renderer, myPlayer);
+		myPlayer->print(renderer);
 
 
 
--- PROItheGame/PROItheGame/BasicFunctions.h
+++ PROItheGame/PROItheGame/BasicFunctions.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <SDL.h>
+#include <fstream>
 #include <iostream>
+#include <string>
 
 #include "Enemy.h"
 #include "HumanPlayer.h"
@@ -9,6 +11,8 @@
 bool init(SDL_Window** window, SDL_Renderer** renderer, const int SCREEN_HEIGHT, const int SCREEN_WIDTH);
 
 void play(SDL_Window* window, SDL_Renderer* renderer, const int SCREEN_HEIGHT, const int SCREEN_WIDTH, const int PLAYER_HEIGHT, const int PLAYER_WIDTH, const double TIME_BETWEEN_FRAMES);
+
+void load(std::string pathToFile, HumanPlayer **myPlayer, std::vector<Obstacle*>*myObstacles, std::vector<Enemy*>*myEnemies);
 
 void clear(SDL_Renderer* rendererToPrintOn);
 
